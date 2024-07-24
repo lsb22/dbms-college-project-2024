aa351c482486651ce86878578516999f565ffeeb
lsb <likith15600@gmail.com>
2024-07-24 20:41:10 +0530

    Implemented new classroom adding feature

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,8 @@ app.get("/classrooms", (req, res) => {
   });
 });
 
+// retriving students from a praticular class
+
 app.get("/classrooms/get/:classroomId", (req, res) => {
   const q = "select * from students where classroom_id = ?";
   const { classroomId } = req.params;
@@ -33,6 +35,8 @@ app.get("/classrooms/get/:classroomId", (req, res) => {
     res.send(data);
   });
 });
+
+// adding new columns -> IA'S
 
 app.post("/classrooms/addColumn/:classroomName", (req, res) => {
   const { classroomName } = req.params;
@@ -79,6 +83,29 @@ app.post("/classrooms/addStudent", (req, res) => {
     (err, data) => {
       if (err) return console.log(err.message);
       return console.log(data);
+    }
+  );
+});
+
+// adding new classrooms
+
+app.post("/classroom/add/NewClassroom", (req, res) => {
+  if (!req.body) {
+    return console.log("body is null");
+  }
+  console.log(req.body);
+  const q = "insert into classrooms values(default,?,?,?,?)";
+  db.query(
+    q,
+    [
+      req.body.year,
+      req.body.semester,
+      req.body.teacher_id,
+      req.body.subject_name,
+    ],
+    (err, data) => {
+      if (err) console.log(err.message);
+      console.log(data);
     }
   );
 });
